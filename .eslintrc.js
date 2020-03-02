9f1aa7baacee92eff1b621f065128b61cc950a3c
Salatiel Queiroz <salatielsqueiroz@gmail.com>
2020-03-02 10:53:26 -0300

    feat: setup eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  "extends": [
+    "cheesecakelabs/react"
+  ],
+  "parser": "babel-eslint",
+  "env": {
+    "browser": true,
+    "node": true,
+    "es6": true,
+    "jest": true
+  },
+  "settings": {
+    "import/resolver": {
+      "webpack": {
+        "config": "gatsby.resolve.js"
+      }
+    }
+  },
+  "rules": {
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        "devDependencies": [
+          "**/*.dev.js",
+          "**/*.test.js",
+          "**/*.stories.js",
+          "server.js",
+          "next.config.js",
+          "postcss.config.js",
+          "webpack.config.*",
+          "storybook/*",
+          ".storybook/**",
+          "stories/**",
+          "stories.**",
+        ]
+        
+      }
+    ],
+    "import/order": [
+      "error",
+      {
+        "newlines-between": "always",
+        "groups": [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index"
+        ]
+      }
+    ]
+  },
+  "globals": {}
+};
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const resolveOptions = require('./gatsby.resolve')
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -25,6 +27,12 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-alias-imports`,
+      options: {
+        ...resolveOptions,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
